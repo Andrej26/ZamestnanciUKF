668e0529939884dbcede6da77f5ec794b6488548
Andrej26 <Andrej26@users.noreply.github.com>
2017-10-17 18:22:25 +0200

    vytvorenie modelu pre databázu

---
+++ app/Model/Zamestnanci.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Zamestnanci extends Model
+{
+    protected $table='zamestnanci';
+
+    protected  $fillable = ['meno', 'priezvisko', 'email', 'heslo', 'vek'];
+}
