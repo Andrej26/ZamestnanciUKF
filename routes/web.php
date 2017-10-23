aaa0c91f08c14f8119a7e1b0f310cd73495fbb1e
Bajlox <tomwarcraftxx@gmail.com>
2017-10-23 23:05:42 +0200

    Dal som template

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/',function (){
+   return view('index');
+});
+
 Route::resource('UKF', 'UKFController');
 
 Route::get('/login', [
