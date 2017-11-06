34c5f73f03a5c68e4529d76f47255301f669ce9a
Andrej <andrejtibensky20@gmail.com>
2017-11-06 23:00:08 +0100

    Oprava 02

--- resources/views/Layouts/master.blade.php
+++ resources/views/Layouts/master.blade.php
@@ -1,12 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Zamestnanci UKF</title>
     <meta charset="utf-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1, user-scalable=yes">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+    <!-- Loading third party fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Roboto:300,400,700|" rel="stylesheet" type="text/css">
+    <link href="{{URL::to('/')}}fonts/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="{{URL::to('/')}}fonts/icomoon/style.css" rel="stylesheet" type="text/css">
+
+    <!-- Loading main css file -->
+    <link rel="stylesheet" href="{{URL::to('/')}}/style.css">
+
+    <!--[if lt IE 9]>
+    <script src="{{URL::to('/')}}/js/ie-support/html5.js"></script>
+    <script src="{{URL::to('/')}}/js/ie-support/respond.js"></script>
+    <![endif]-->
+
+    <style>
+
+        .zarovnanie{
+            border: 1px solid #ccc;
+            text-align: center;
+        }
+        .but01{
+            margin:3px;
+        }
+
+        .img-thumbnail_01{
+            display: inline-block;
+            width: 330px;
+            height: 310px;
+            margin: 1px;
+        }
+        .odh {
+            text-align: right;
+        }
+
+    </style>
+
 </head>
 
 <body>
@@ -14,8 +56,22 @@
     <div class="page-header">
         @yield('header')
     </div>
+
     @yield('content')
+
 </div>
+
+<footer class="site-footer">
+    <p>Copyright &copy; 2017 Company Name. Designed by A-Team. All rights reserved</p>
+
+    <div class="social-links">
+        <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+        <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+        <a href="#" class="pinterest"><i class="fa fa-pinterest"></i></a>
+    </div>
+</footer>
+
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -11,39 +11,68 @@
 |
 */
 
-use GuzzleHttp\Client;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function ()
-{
+Route::get('/', function () {
     return view('welcome');
-
 });
 
-Route::get('/',function (){
-   return view('index');
+Route::get('UKF', 'UKFController@index')->name('ukf');
+
+
+
+// Admin tables!!!!!
+//////////////////////////////////////////////////////////////////////
+
+Route::get('Zmena_stavu0/{id}',[
+    'as'=>'zmena_stavu0', 'uses'=>'DBControllers\DBZamestnanci@hide'
+]);
+
+Route::get('Zmena_stavu1/{id}',[
+    'as'=>'zmena_stavu1', 'uses'=>'DBControllers\DBZamestnanci@unhide'
+]);
+
+//dalsie tabulkz este nie su...
+
+//////////////////////////////////////////////////////////////////////////
+
+Route::get('/picture/{filename}', function ($filename)
+{
+    $path = storage_path() . '/picture/' . $filename;
+
+    if(!File::exists($path)) abort(404);
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+    return $response;
+})->name('picture');
+
+// Prihlasenie
+////////////////////////////////////////////////////////////////
+Auth::routes();
+
+Route::prefix('Zamestnanec')->group(function(){
+
+    //Prihlasenie zamestnanec
+    Route::get('/logout', 'ZamestnanecController@logout')->name('zames.logout');
+    Route::post('/login', 'Auth\ZamestnanecLoginController@login')->name('zames.login');
+    Route::get('/', 'ZamestnanecController@index')->name('zames.dashboard');
 });
 
-Route::resource('UKF', 'UKFController');
+Route::prefix('Admin')->group(function() {
 
-Route::get('/login', [
-    'as' => 'login', 'uses' => 'UKFController@login'
-]);
+    //Tablulka zamestnancov
+    Route::get('/TabZamestnanci', 'DBControllers\DBZamestnanci@index')->name('TabZamestnanci.index');
+    Route::post('/TabZamestnanci', 'DBControllers\DBZamestnanci@store')->name('TabZamestnanci.store');
+    Route::get('/TabZamestnanci/Vytvorenie_zamestnanca', 'DBControllers\DBZamestnanci@create')->name('TabZamestnanci.create');
+    Route::get('/TabZamestnanci/{id}', 'DBControllers\DBZamestnanci@show')->name('TabZamestnanci.show');
+    Route::match(['put','patch'],'/TabZamestnanci/{id}', 'DBControllers\DBZamestnanci@update')->name('TabZamestnanci.update');
+    Route::get('/TabZamestnanci/{id}/Úprava_zamestnanca', 'DBControllers\DBZamestnanci@edit')->name('TabZamestnanci.edit');
 
-Route::post('/prihlas', [
-    'as' => 'prihlas', 'uses' => 'UKFController@prihlas'
-]);
-
-Route::get('/dbApi', [
-    'as' => 'dbApi', 'uses' => 'DbConnectTest@CopyData'
-]);
-
+    //Prihlasenie admin
+    Route::get('/logout', 'AdminController@logout')->name('admin.logout');
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    Route::get('/', 'AdminController@index')->name('admin.dashboard');
+});
