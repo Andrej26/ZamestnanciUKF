1ab03a6167253af24e7ba77a633ee0682d642e12
Andrej <andrejtibensky20@gmail.com>
2017-11-12 18:45:50 +0100

    Zmena obrázkov databázy

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,19 +7,19 @@
 @section('content')
     <div class="zarovnanie">
         <a class="btn btn-outline-dark but01" href="{{ url('Admin/TabZamestnanci')}}">
-            <input  src="{{URL::to('/')}}/images/skills-013.png" alt="Tabulka01" type="image" class="img-thumbnail_01" />
+            <input  src="{{URL::to('/')}}/images/zamestnanci.png" alt="Tabulka01" type="image" class="img-thumbnail_01" />
         </a>
 
         <a class="btn btn-outline-dark but01" href="{{ url('Admin/TabRola') }}">
-            <input  src="{{URL::to('/')}}/images/skills-013.png" alt="Tabulka01" type="image" class="img-thumbnail_01" />
+            <input  src="{{URL::to('/')}}/images/rolapouzivatela.png" alt="Tabulka01" type="image" class="img-thumbnail_01" />
         </a>
 
         <a class="btn btn-outline-dark but01" href="{{ url('Admin/TabFakulta') }}">
-            <input  src="{{URL::to('/')}}/images/skills-013.png" alt="Tabulka01" type="image" class="img-thumbnail_01" />
+            <input  src="{{URL::to('/')}}/images/fakulta.png" alt="Tabulka01" type="image" class="img-thumbnail_01" />
         </a>
 
         <a class="btn btn-outline-dark but01" href="{{ url('Admin/TabKatedra') }}">
-            <input  src="{{URL::to('/')}}/images/skills-013.png" alt="Tabulka01" type="image" class="img-thumbnail_01" />
+            <input  src="{{URL::to('/')}}/images/katedra.png" alt="Tabulka01" type="image" class="img-thumbnail_01" />
         </a>
 
     </div>
