7fd22bbb49c37ea2672c53750b735a5630ffc07e
Andrej <andrejtibensky20@gmail.com>
2017-12-10 23:23:07 +0100

    uprava 04

--- app/Http/Controllers/ChartsC.php
+++ app/Http/Controllers/ChartsC.php
@@ -60,30 +60,6 @@ class ChartsC extends Controller
         return view('showCharts_zam',['chart'=> $chart, 'idfakulty'=>$idfakulty]);
     }
 
-    public function spojenie2tabuliek()
-    {
-        $pom=[];
-
-        $kat01 = Katedra::select('katedra.*', 'Fakulta.nazov as nazov01')
-            ->join('Fakulta', 'idFakulta', '=', 'Fakulta_idFakulta')
-            ->get();
-
-        $zames = Zamestnanec::select('*')
-            ->join('rolaPouzivatela', 'idrolaPouzivatela', '=', 'rolaPouzivatela_idrolaPouzivatela')
-            ->join('katedra', 'idKatedra', '=', 'Katedra_idKatedra')
-            ->get();
-
-        foreach ($zames as $zam):
-            foreach ($kat01 as $kat):
-                if($kat['nazov'] == $zam['nazov']) {
-                    $pom[] = ['id' => $zam->idzamestnanec, 'meno'=> $zam->meno, 'email'=> $zam->email, 'katedra'=> $zam->nazov,
-                        'rola'=> $zam->rola, 'profil'=> $zam->profil,'idFakulta'=>$kat->Fakulta_idFakulta,'fakulta'=>$kat->nazov01];
-                }
-            endforeach;
-        endforeach;
-
-        return (object) $pom;
-    }
 
 
 }
