2695452735bead4f0908b63d4cd5f90eb64a94d1
pando7 <patrikando1995@gmail.com>
2017-12-10 22:35:41 +0100

    Uprava grafov

--- app/Http/Controllers/ChartsC.php
+++ app/Http/Controllers/ChartsC.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Model\Katedra;
+use App\Model\Projekt;
+use App\Model\Publikacia;
 use App\Model\Zamestnanec;
 use ConsoleTVs\Charts\Facades\Charts;
 use Illuminate\Support\Facades\DB;
@@ -14,29 +16,25 @@ class ChartsC extends Controller
 
     public function index($idfakulty)
     {
-
-        // $tmp = $this->spojenie2tabuliek();
-
-        /* $users = Zamestnanec::where(DB::raw("(DATE_FORMAT(created_at,'%Y'))"),date('Y'))
-
-             ->get();*/
-
-        /*$users = Zamestnanec::where(DB::raw("katedra_idkatedra"),date('Y'))
-
-            ->get();*/
-
-        /*$users = $this->spojenie2tabuliek();*/
-
         $zames = Zamestnanec::select('Zamestnanec.*','Katedra.nazov as katedra')
             ->join('Katedra', 'idKatedra', '=', 'Katedra_idKatedra')
             ->where('katedra.Fakulta_idFakulta',$idfakulty)
             ->get();
 
-        $chart = Charts::database($zames, 'pie', 'highcharts')
+        $publikacie = Publikacia::select('*')
+            ->join('zamestnanec','idzamestnanec','=','Zamestnanec_idzamestnanec')
+            ->get();
 
-            ->title("Rozdelenie podľa katedier")
+        $projekty = Projekt::select('projekt.*', 'zamestnanec.Katedra_idkatedra as katedra')
+            ->join('zamestnanec', 'Zamestnanec_idzamestnanec', '=', 'idzamestnanec')
+            ->orderBy('zaciatok')
+            ->get();
 
-            ->elementLabel("Total Users")
+        $chartZamKat = Charts::database($zames, 'pie', 'highcharts')
+
+            ->title("Rozdelenie zamestnancov podľa katedier")
+
+            ->elementLabel("Počet zamestnancov podľa katedry")
 
             ->dimensions(1000, 500)
 
@@ -44,37 +42,38 @@ class ChartsC extends Controller
 
             ->groupBy('katedra');
 
-        return view('showCharts',['chart'=> $chart, 'idfakulty'=>$idfakulty]);
+
+        $chartProjectsCount = Charts::database($projekty, 'bar', 'highcharts')
+
+            ->title("Počet uskutočnených projektov")
+
+            ->elementLabel("Počet projektov")
+
+            ->dimensions(1000, 500)
+
+            ->responsive(false)
+
+            ->groupBy('zaciatok');
+
+
+        $chartPubCount = Charts::database($publikacie, 'pie', 'highcharts')
+
+            ->title("Rozdelenie publikácií podľa katedier")
+
+            ->elementLabel("Počet publikácií")
+
+            ->dimensions(1000, 500)
+
+            ->responsive(false)
+
+            ->groupBy('zamestnanec.Katedra_idkatedra');
+
+
+
+
+        return view('showCharts',['chartZamKat'=> $chartZamKat, 'chartProjectsCount' => $chartProjectsCount, 'chartPubCount' => $chartPubCount,'idfakulty'=>$idfakulty]);
 
     }
 
-    public function showAll()
-    {
 
-    }
-
-    public function spojenie2tabuliek()
-    {
-        $pom=[];
-
-        $kat01 = Katedra::select('katedra.*', 'Fakulta.nazov as nazov01')
-            ->join('Fakulta', 'idFakulta', '=', 'Fakulta_idFakulta')
-            ->get();
-
-        $zames = Zamestnanec::select('*')
-            ->join('rolaPouzivatela', 'idrolaPouzivatela', '=', 'rolaPouzivatela_idrolaPouzivatela')
-            ->join('katedra', 'idKatedra', '=', 'Katedra_idKatedra')
-            ->get();
-
-        foreach ($zames as $zam):
-            foreach ($kat01 as $kat):
-                if($kat['nazov'] == $zam['nazov']) {
-                    $pom[] = ['id' => $zam->idzamestnanec, 'meno'=> $zam->meno, 'email'=> $zam->email, 'katedra'=> $zam->nazov,
-                        'rola'=> $zam->rola, 'profil'=> $zam->profil,'idFakulta'=>$kat->Fakulta_idFakulta,'fakulta'=>$kat->nazov01];
-                }
-            endforeach;
-        endforeach;
-
-        return (object) $pom;
-    }
 }
